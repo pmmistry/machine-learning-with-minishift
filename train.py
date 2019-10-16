1d110e8b54d52506fa1d5052a6961c88b53a41c7
Pooja <poojamistry22@gmail.com>
2019-10-16 19:25:56 -0400

    updates

--- train.py
+++ train.py
@@ -18,7 +18,8 @@ def start():
     # the data, split between train and test sets
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-      if K.image_data_format() == 'channels_first':
+
+    if K.image_data_format() == 'channels_first':
         x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
         x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
         input_shape = (1, img_rows, img_cols)
@@ -39,7 +40,7 @@ def start():
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
-     model = Sequential()
+    model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
                     activation='relu',
                     input_shape=input_shape))
@@ -52,8 +53,8 @@ def start():
     model.add(Dense(num_classes, activation='softmax'))
 
     model.compile(loss=keras.losses.categorical_crossentropy,
-        optimizer=keras.optimizers.Adadelta(),
-        metrics=['accuracy'])
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
 
     model.fit(x_train, y_train,
             batch_size=batch_size,
@@ -65,3 +66,4 @@ def start():
     print('Test accuracy:', score[1])
 
     model.save('mnist.h5')
+    
